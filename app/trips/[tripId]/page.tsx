ae1f7a194a07eec2eb328347010074df7334dfb4
nafizurr <nafizurr@buffalo.edu>
2025-09-29 00:38:32 -0400

    Refactor trip detail page and enhance new location functionality with geocoding and form submission

--- app/trips/[tripId]/page.tsx
+++ app/trips/[tripId]/page.tsx
@@ -2,25 +2,29 @@ import { auth } from "@/auth";
 import TripDetailClient from "@/components/trip-details";
 import { prisma } from "@/lib/prisma";
 
+export default async function TripDetail({
+  params,
+}: {
+  params: Promise<{ tripId: string }>;
+}) {
+  const { tripId } = await params;
 
-export default async function TripDetails({params,}: {params: Promise<{tripId: string}>}) {
-    const {tripId} = await params;
-    
-    const session = await auth();
-    
-    if (!session) {
-        return <div>Please Sign In.</div>
-    }
+  const session = await auth();
 
-    const trip = await prisma.trip.findFirst({
-        where: {id: tripId}
-    })
-    //querying from the server and make use of the client actions
+  if (!session) {
+    return <div> Please sign in.</div>;
+  }
 
-    if(!trip) {
-        return <div>Trip Not Found</div>
-    }
+  const trip = await prisma.trip.findFirst({
+    where: { id: tripId, userId: session.user?.id },
+    include: { locations: true },
+  });
 
-    return <TripDetailClient trip = {trip}/>
+  console.log(trip);
 
+  if (!trip) {
+    return <div> Trip not found.</div>;
+  }
+
+  return <TripDetailClient trip={trip} />;
 }
--- components/trip-details.tsx
+++ components/trip-details.tsx
@@ -1,23 +1,109 @@
 "use client";
 
-import {Trip} from "@/app/generated/prisma"
+import {Trip, Location} from "@/app/generated/prisma"
 import Image from "next/image"
+import { Button } from "./ui/button";
+import {Calendar, MapPin, Plus} from 'lucide-react'
+import Link from "next/link";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+import { useState } from "react";
+
+type TripWithLocation = Trip & {
+    locations: Location[];
+}
 
 interface TripDetailClientProps {
-    trip: Trip;
+    trip: TripWithLocation;
 }
 //declare as a client component
 export default function TripDetailClient({trip}: TripDetailClientProps) {
-    return <div className = "container mx-auto px-4 py-8 space-y-8">
+    const [activeTab, setActiveTab] = useState("overview")
+
+    return (
+    <div className = "container mx-auto px-4 py-8 space-y-8">
         {trip.imageUrl && (
             <div className="w-full h-72 md:h-96 overflow-hidden rounded-xl shadow-lg relative">
                 <Image 
                     src = {trip.imageUrl} 
                     alt = {trip.title} 
-                    className = "object" 
+                    className = "object-cover" 
                     fill 
                     priority/>
             </div>
         )}
+        <div className="bg-white p-6 shadow rounded-lg flex flex-col md:flex-row justify-between items-start md:items-center">
+        <div>
+          <h1 className="text-4xl font-extrabold text-gray-900">
+            {" "}
+            {trip.title}
+          </h1>
+
+          <div className="flex items-center text-gray-500 mt-2">
+            <Calendar className="h-5 w-5 mr-2" />
+            <span className="text-lg">
+              {trip.startDate.toLocaleDateString()} - {" "}
+              {trip.endDate.toLocaleDateString()}
+            </span>
+          </div>
+        </div>
+        <div className="mt-4 md:mt-0">
+          <Link href={`/trips/${trip.id}/itinerary/new`}>
+            <Button>
+              {" "}
+              <Plus className="mr-1 h-5 w-5" /> Add Location
+            </Button>
+          </Link>
+        </div>
+      </div>
+ 
+      <div className="bg-white p-6 shadow rounded-lg">
+        <Tabs value={activeTab} onValueChange={setActiveTab}>
+            <TabsList className="mb-6">
+                <TabsTrigger value="overview" className="text-lg">Overview</TabsTrigger>
+                <TabsTrigger value="itinerary" className="text-lg">Itinerary</TabsTrigger>
+                <TabsTrigger value="map" className="text-lg">Map</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value = "overview" className="space-y-6">
+                <div className="grid md:grid-cols-2 gap-6">
+                    <div>
+                        <h2 className="text-2xl font-semibold mb-4"> Trip Summary</h2>
+                            <div className="space-y-4">
+                            <div className="flex items-start">
+                                <Calendar className="h-6 w-6 mr-3 text-gray-500" />
+                                <div>
+                                    <p className="font-medium text-gray-700"> Dates</p>
+                                    <p className="text-sm text-gray-500">
+                                        {trip.startDate.toLocaleDateString()} -{" "}
+                                        {trip.endDate.toLocaleDateString()}
+                                        <br />
+                                        {`${Math.round(
+                                        (trip.endDate.getTime() - trip.startDate.getTime()) /
+                                            (1000 * 60 * 60 * 24)
+                                        )} days(s)`}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="flex items-start">
+                                <MapPin className = "h-6 w-6 mr-3 text-gray-500"></MapPin>
+                                <div>
+                                    <p>Destinations</p>
+                                    <p>{trip.locations.length} {" "}
+                                        {trip.locations.length === 1 ? "location" : "locations"}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="">
+
+                    </div>
+                </div>
+            </TabsContent>
+        </Tabs>
+      </div>
+
+
     </div>
-}
+)}
